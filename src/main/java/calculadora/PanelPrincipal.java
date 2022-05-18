e6042b755c242581a6c5269b6a9a25f50bc3d527
dferfer235 <dferfer235@g.educaand.es>
2022-05-18 14:24:05 +0200

    Acciones de los botones de la Calculadora

--- src/main/java/calculadora/PanelPrincipal.java
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -17,6 +17,9 @@ public class PanelPrincipal extends JPanel implements ActionListener {
     private PanelBotones botonera;
     private JTextArea areaTexto;
     private int tipoOperacion;
+    boolean suma = false, resta = false, multiplicar = false, dividir = false;
+    int num1 = 0, num2 = 0;
+    int turno = 1;
 
     // Constructor
     public PanelPrincipal() {
@@ -44,14 +47,74 @@ public class PanelPrincipal extends JPanel implements ActionListener {
 
     }
 
+    private void limpiar() {
+        num1 = 0;
+        num2 = 0;
+        turno = 1;
+        suma = false;
+        resta = false;
+        multiplicar = false;
+        dividir = false;
+    }
+
     @Override
     public void actionPerformed(ActionEvent ae) {
-        // Se obtiene el objeto que desencadena el evento
+
         Object o = ae.getSource();
         // Si es un botón
         if (o instanceof JButton) {
             System.out.println(((JButton) o).getText());
             areaTexto.setText(((JButton) o).getText());
+
+            try {
+                if (turno == 1) {
+                    num1 = Integer.valueOf((((JButton) o).getText()));
+                } else {
+                    num2 = Integer.valueOf((((JButton) o).getText()));
+                }
+
+            } catch (Exception e) {
+                if (((JButton) o).getText().equalsIgnoreCase("+")) {
+                    suma = true;
+                    turno++;
+                }
+                if (((JButton) o).getText().equalsIgnoreCase("-")) {
+                    resta = true;
+                    turno++;
+                }
+                if (((JButton) o).getText().equalsIgnoreCase("*")) {
+                    multiplicar = true;
+                    turno++;
+                }
+                if (((JButton) o).getText().equalsIgnoreCase("/")) {
+                    dividir = true;
+                    turno++;
+                }
+            }
+
+            if (((JButton) o).getText().equalsIgnoreCase("=")) {
+                if (suma) {
+                    int RSuma = num1 + num2;
+                    System.out.println("Resultado:" + RSuma);
+                }
+                if (resta) {
+                    int RResta = num1 - num2;
+                    System.out.println("Resultado:" + RResta);
+                }
+                if (multiplicar) {
+                    int RMultiplicacion = num1 * num2;
+                    System.out.println("Resultado:" + RMultiplicacion);
+                }
+                if (dividir) {
+                    int RDivision = num1 / num2;
+                    System.out.println("Resultado:" + RDivision);
+                }
+
+                limpiar();
+            }
+            if (((JButton) o).getText().equalsIgnoreCase("c")) {
+                limpiar();
+            }
         }
 
         // RESTO DEL CÓDIGO DE LA LÓGICA DE LA CALCULADORA
